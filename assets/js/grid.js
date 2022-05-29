35e5e7999af59ab525a0a0940537c2a80fd2ff0d
reza sharifi <32232100+mshgold@users.noreply.github.com>
2022-05-29 18:59:16 +0430

    level-one

---
+++ assets/js/grid.js
@@ -0,0 +1,29 @@
+
+var humber = document.getElementById("icon-humber");
+function showmenu() {
+
+    var mhumber = document.getElementById("navbar-md");
+    if (mhumber.className === "navbar-md d-md-none d-none") {
+        mhumber.className += "d-show"
+    } else {
+        mhumber.className = "navbar-md d-md-none d-none"
+    }
+}
+showpro();
+function showslid2() {
+   var slid1 = document.getElementById("slide");
+   var dot=document.getElementById("dot1"); 
+   var dot2=document.getElementById("dot2"); 
+   slid1.style.marginLeft = "-968px";
+   dot.style.backgroundColor="#dcdcdc";
+   dot2.style.backgroundColor="#1b1a17";
+}
+function showslid1() {
+   var slid1 = document.getElementById("slide");
+   var dot=document.getElementById("dot1"); 
+   debugger; 
+    slid1.style.marginLeft = "0px";
+    var dot=document.getElementById("dot1"); 
+    dot2.style.backgroundColor="#dcdcdc";
+    dot.style.backgroundColor="#1b1a17";
+}
