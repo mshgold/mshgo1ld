9686abe70fa485f5e579a5f4331828de4535132f
reza sharifi <32232100+mshgold@users.noreply.github.com>
2022-06-04 18:57:19 +0430

    Update grid.js

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -66,6 +66,7 @@ function addbutton(btncart) {
     const btn = document.createElement("button");
     const btnadd = document.createElement("button");
     const btnless = document.createElement("button");
+    const span = document.createElement("span");
     var items = document.getElementsByClassName("product-item");
     for (var i = 0; i < 8; i++) {
         if (i == btncart.id) {
@@ -75,14 +76,14 @@ function addbutton(btncart) {
             btn.style.padding = " 0 .2rem";
             btn.appendChild(img);
             div1.appendChild(btn);
-            const span = document.createElement("span");
+
             span.className = "span-counter";
             span.innerHTML = "1";
             div1.appendChild(span);
             btnadd.className = "bnt-counter";
             btnadd.innerHTML = "+";
             div1.appendChild(btnadd);
-           items[i].appendChild(div1);
+            items[i].appendChild(div1);
             btncart.style.display = "none";
         }
     }
@@ -91,20 +92,36 @@ function addbutton(btncart) {
         btncart.style.display = "flex";
         div1.style.display = "none";
     }
+    // debugger;
+
     btnadd.addEventListener("click", addcart);
+    var x = 0, y = 1;
+
     function addcart() {
         // btn.style.display = "none";
-        btnless.className="bnt-counter";
-        btnless.innerHTML="-";
+        btnless.className = "bnt-counter";
+        btnless.innerHTML = "-";
         // btnless.display.float="left";
-        
-        
-        div1.replaceChild(btnless,btn);
-        // div1.removeChild(btnadd);
-        // div1.removeChild(span.chi);
-        // div1.appendChild(btnless);
-        // div1.appendChild(span);
-        // div1.appendChild(btnadd);
 
+        console.log(x);
+        // debugger;
+        if (!(div1.firstChild.isSameNode(btnless))) {
+            div1.replaceChild(btnless, btn);
+          
+        }
+        y += 1;
+        x += 1;
+        span.innerHTML = y.toString();
+        x++;
+
+    }
+    btnless.addEventListener("click", lesscart);
+    function lesscart() {
+        if(y==2){
+            div1.replaceChild(btn,btnless);
+        }
+        console.log(y);
+        y-=1;
+        span.innerHTML = y.toString();
     }
 }
